eba92a088d5f4570eeb255b3516a7b0a669ea964
“Haruka” <harusaki812@icloud.com>
2025-07-31 15:36:13 +0900

    初回コミット: NyanDoのAppStore公開に向けた初期バージョン

---
+++ NyanDoUITests/NyanDoUITests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUTests.swift
+//  HelloSwiftUTests
+//
+//  Created by 今井悠翔 on 2025/07/02.
+//
+
+import XCTest
+@testable import NyanDo
+
+final class HelloSwiftUTests: XCTestCase {
+
+    func testExample() async throws {
+        // ここにテスト内容を書く
+        XCTAssertTrue(true) // 仮のテスト
+    }
+}
